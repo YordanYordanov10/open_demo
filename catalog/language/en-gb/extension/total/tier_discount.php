ca2afa4b5e6d9271251ff222b323a048dc63bed8
Yordan <dakadelpiero@gmail.com>
2026-02-13 17:01:10 +0200

    finish extension for tier_discount

--- catalog/language/en-gb/extension/total/tier_discount.php
+++ catalog/language/en-gb/extension/total/tier_discount.php
@@ -1,3 +1,20 @@
 <?php
 // Heading
 $_['heading_title']    = 'Tier_Discount';
+
+// Text
+$_['text_extension']   = 'Extensions';
+$_['text_success']     = 'Success: You have modified tier discount total!';
+$_['text_edit']        = 'Edit Tier Discount Total';
+$_['text_guest_login_to_discount'] = 'Login to unlock additional discounts';
+$_['text_vip_customer'] = 'VIP Customer';
+$_['text_tier_discount'] = 'Tier Discount %s;';
+
+// Entry
+$_['entry_status']     = 'Status';
+$_['entry_sort_order'] = 'Sort Order';
+$_['entry_discount_tier1_min'] = 'Tier 1 Minimum';
+$_['entry_discount_tier1_percent'] = 'Tier 1 Percent';
+$_['entry_discount_tier2_min'] = 'Tier 2 Minimum';
+$_['entry_discount_tier2_percent'] = 'Tier 2 Percent';  
+$_['entry_discount_vip_percent'] = 'VIP Percent';
--- catalog/model/extension/total/tier_discount.php
+++ catalog/model/extension/total/tier_discount.php
@@ -2,5 +2,80 @@
 
 class ModelExtensionTotalTierDiscount extends Model{
 
-    
+    public function getTotal($total) {
+        $this->load->language('extension/total/tier_discount');
+        $this->load->model('account/order');
+
+        if (!$this->config->get('total_tier_discount_status')) {
+            return;
+        }
+
+        $subtotal = $this->cart->getSubTotal();
+        $discount = 0;     
+        $is_vip = false;
+        
+
+
+        if($this->customer->isLogged()){
+            $total_spent = $this->model_account_order->getOrderTotalsById($this->customer->getId());
+
+		 if( (float)$total_spent >= 1000 ) {
+		  $is_vip = true;
+         }
+		}
+
+        $discount_tier1_min = (float)$this->config->get('total_tier_discount_tier1_min');
+        $discount_tier2_min = (float)$this->config->get('total_tier_discount_tier2_min');
+
+        $discount_tier1_percent = (float)$this->config->get('total_tier_discount_tier1_percent');
+        $discount_tier2_percent = (float)$this->config->get('total_tier_discount_tier2_percent');
+        $discount_vip_percent = (float)$this->config->get('total_tier_discount_vip_percent');
+       
+        $totalDiscount = 0;
+
+        if ($this->customer->isLogged()) {
+
+        if ($is_vip) {
+           
+            if($subtotal >= $discount_tier2_min) {
+                $totalDiscount = $discount_vip_percent + $discount_tier2_percent;
+                $discount = $subtotal * ($totalDiscount  / 100);
+            } elseif ($subtotal >= $discount_tier1_min) {
+                $totalDiscount = $discount_vip_percent + $discount_tier1_percent;
+                $discount = $subtotal * ($totalDiscount  / 100);
+            
+            } else {
+                $totalDiscount = $discount_vip_percent;
+                $discount = $subtotal * ($totalDiscount / 100);
+               
+            }
+        }
+
+        } else {
+
+        if ($subtotal >= $discount_tier2_min) {
+                $totalDiscount = $discount_tier2_percent;
+                $discount = $subtotal * ($totalDiscount / 100);
+            } elseif ($subtotal >= $discount_tier1_min) {
+                $totalDiscount = $discount_tier1_percent;
+                 $discount = $subtotal * ($totalDiscount / 100);
+               
+            }
+        }
+
+        if ($discount > 0) {
+        $total['totals'][] = array(
+            'code' => 'tier_discount',
+            'title' => sprintf( $this->language->get('text_tier_discount'),$this->currency->format($totalDiscount, $this->session->data['currency'])),
+            'value' => -$discount,
+            'sort_order' => $this->config->get('total_tier_discount_sort_order')
+        );
+        $total['total'] -= $discount;
+        
+        }
+
+       
+
+    }    
+        
 }
